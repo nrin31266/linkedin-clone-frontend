4cbb6dc36f11f103ba53fa2ad325e7d4706a5445
nrin31266 <162881731+nrin31266@users.noreply.github.com>
2025-03-14 23:15:15 +0700

    handleApi

--- src/configs/handleAPI.ts
+++ src/configs/handleAPI.ts
@@ -1,33 +1,54 @@
 import axios from "axios";
-import { ErrorResponse } from "../errors/ErrorResponse";
 
-import axiosInstance from "./axiosConfig";
+const API_BASE_URL = import.meta.env.VITE_API_URL;
 
-interface RequestParams<T> {
+interface RequestParams<T, B = unknown> {
   endpoint: string;
-  body?: any;
+  body?: B;
   method?: "post" | "get" | "delete" | "put";
   onSuccess?: (data: T) => void;
   onFailure?: (error: string) => void;
   onFinally?: () => void;
+  isAuthenticated?: boolean;
 }
 
-const handleAPI  = async <T>({
+
+
+const handleAPI = async <T, B = unknown>({
   endpoint,
   body,
   method = "get",
   onFailure,
   onSuccess,
-  onFinally
-}: RequestParams<T>): Promise<void> => {
+  onFinally,
+  isAuthenticated = true
+}: RequestParams<T, B>): Promise<void> => {
   try {
-    const res = await axiosInstance(endpoint, { method, data: body });
-    onSuccess && onSuccess(res.data.data);
+    const headers: Record<string, string> = {
+      "Content-Type": "application/json",
+    };
+
+    if (isAuthenticated) {
+      const token = localStorage.getItem("token");
+      if (token) {
+        headers.Authorization = `Bearer ${token}`;
+      }
+    }
+
+    const res = await axios({
+      url: endpoint,
+      method,
+      data: body,
+      headers,
+      baseURL: API_BASE_URL,
+      timeout: 5000
+    });
+
+    onSuccess?.(res.data.data);
   } catch (error) {
-    
     if (onFailure) {
       if (axios.isAxiosError(error) && error.response?.data) {
-        const { code, message, details } = error.response.data;
+        const { code, message } = error.response.data;
         if (typeof code === "number" && typeof message === "string") {
           onFailure(message);
           return;
@@ -35,9 +56,8 @@ const handleAPI  = async <T>({
       }
       onFailure("An unexpected error occurred.");
     }
-    
-  } finally{
-    onFinally && onFinally();
+  } finally {
+    onFinally?.();
   }
 };
 
